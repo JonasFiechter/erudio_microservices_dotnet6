b3b9b8aa55a655f9de80ef0aaef61f38fd3367d3
Jonas Fiechter <jonasrfiechter@gmail.com>
2023-02-19 02:12:07 -0300

    upgrade calculator with some operations

--- rest_calc_api/rest_calc_api/Controllers/CalculatorController.cs
+++ rest_calc_api/rest_calc_api/Controllers/CalculatorController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Emit;
 
 namespace rest_calc_api.Controllers
 {
@@ -14,27 +15,73 @@ namespace rest_calc_api.Controllers
             _logger = logger;
         }
 
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        [HttpGet("{operation}/{firstNumber}/{secondNumber}")]
+        
+        public IActionResult Get(string operation, string firstNumber, string secondNumber)
         {
 
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
-                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
+
+                var firstConvertedNumber = ConvertToDecimal(firstNumber);
+                var secondConvertedNumber = ConvertToDecimal(secondNumber);
+
+                string result = CheckOperation(operation, firstConvertedNumber, secondConvertedNumber);
+
+                return Ok(result);
+
             }    
 
             return BadRequest("Invalid Input");
         }
 
+
+        public string CheckOperation(string operation, decimal firstNumber, decimal secondNumber)
+        {
+            if (operation == "sum")
+            {
+                decimal sumResult = firstNumber + secondNumber;
+                return sumResult.ToString();
+            }
+
+            if (operation == "multiply") 
+            {
+                decimal multiplyResult = firstNumber * secondNumber;
+                return multiplyResult.ToString();
+            }
+
+            if (operation == "divide")
+            {
+                decimal divideResult = firstNumber / secondNumber;
+                return divideResult.ToString();
+            }
+
+            else
+            {
+                string checkResult = "Invalid Operation";
+                return checkResult;
+            }
+        }
+
         private bool IsNumeric(string number)
         {
+            if (double.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out double result)) 
+            {
+                return true;
+            };
             return false;
         }
 
         private decimal ConvertToDecimal(string number) 
         {
-            throw new NotImplementedException();    
+
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            
+            return 0;    
         }
     }
 }
