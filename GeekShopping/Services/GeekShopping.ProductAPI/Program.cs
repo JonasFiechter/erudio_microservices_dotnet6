f4801d9cd0aa53c75e4445d5badb2f575f5eb4a5
Jonas Fiechter <jonasrfiechter@gmail.com>
2023-03-19 00:40:28 -0300

    updates, structure, models

--- GeekShopping/Services/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/Services/GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,17 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// DB connection
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(
+    options => options.UseMySql(
+        connection, new MySqlServerVersion(new Version(8, 0, 31))));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
