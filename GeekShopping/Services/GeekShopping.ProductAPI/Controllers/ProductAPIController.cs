15dc3eedb83201fcd0c00f9640498dc5354e83bf
Jonas Fiechter <jonasrfiechter@gmail.com>
2023-03-21 02:36:49 -0300

    even with warnings API is working

---
+++ GeekShopping/Services/GeekShopping.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPIController.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductAPIController : ControllerBase
+    {
+
+        private readonly ILogger<ProductAPIController> _logger;
+
+        public ProductAPIController(ILogger<ProductAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("api/{param}")]
+        public IActionResult Get(string param)
+        {
+            return Ok("Working");
+        }
+    }
+}
