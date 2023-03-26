f995d9b50dd02518f2a7aa112a618397b54d5539
Jonas Fiechter <jonasrfiechter@gmail.com>
2023-03-26 01:13:30 -0300

    Data seed migration succeded (migration was customized to not override error product table)

--- GeekShopping/Services/GeekShopping.ProductAPI/Migrations/20230326035609_ModelSeedProductAPI.Designer.cs
+++ GeekShopping/Services/GeekShopping.ProductAPI/Migrations/20230326035609_ModelSeedProductAPI.Designer.cs
@@ -2,6 +2,7 @@
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.ProductAPI.Migrations
 {
     [DbContext(typeof(MySQLContext))]
-    partial class MySQLContextModelSnapshot : ModelSnapshot
+    [Migration("20230326035609_ModelSeedProductAPI")]
+    partial class ModelSeedProductAPI
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
